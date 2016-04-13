7f4ddc186b9961a5fc0e20ae24ba9a0e4500151b
lijiang <lijiang@yado.com>
2016-04-13 13:53:14 +0800

    映射测试

---
+++ src/com/legalmall/user/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.legalmall.user.mapper;
+
+import com.legalmall.user.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/com/legalmall/user/model/User.java
@@ -0,0 +1,145 @@
+package com.legalmall.user.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer userId;
+
+    private String userAccount;
+
+    private String userPassword;
+
+    private String userName;
+
+    private String userSex;
+
+    private String industry;
+
+    private String education;
+
+    private String headImg;
+
+    private String userWechat;
+
+    private String userEmail;
+
+    private String userQq;
+
+    private String phoneNum;
+
+    private Date registerTime;
+
+    private String memerId;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(String userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(String userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public String getUserWechat() {
+        return userWechat;
+    }
+
+    public void setUserWechat(String userWechat) {
+        this.userWechat = userWechat;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserQq() {
+        return userQq;
+    }
+
+    public void setUserQq(String userQq) {
+        this.userQq = userQq;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public Date getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(Date registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public String getMemerId() {
+        return memerId;
+    }
+
+    public void setMemerId(String memerId) {
+        this.memerId = memerId;
+    }
+}
